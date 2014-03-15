ad55ee803168c8a93c9e16ac27170da35835710a
Chris <chris@codecraft.co.uk>
2014-03-15 02:40:28 +0000

    Update Schemas
    Different DB Schema

--- schema/default-test-data-only.sql
+++ schema/default-test-data-only.sql
@@ -2,7 +2,7 @@
 SQLyog Enterprise v11.24 (64 bit)
 MySQL - 5.0.67-community-nt : Database - office_league
 *********************************************************************
-*/
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -12,33 +12,33 @@ MySQL - 5.0.67-community-nt : Database - office_league
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*Data for the table `competition` */
-
-insert  into `competition`(`comp_id`,`comp_name`,`comp_owner`) values (1,'SSDM Premier',1),(2,'SSDM Championship',1),(3,'SSDM League One',1),(4,'SSDM League Two',1),(5,'SSDM Cup',1);
-
 /*Data for the table `competition_fixture` */
-
-insert  into `competition_fixture`(`fix_id`,`fix_season`,`fix_competition`,`fix_round`,`fix_home_team`,`fix_away_team`,`fix_played`,`fix_home_score`,`fix_away_score`) values (1,1,1,1,1,2,0,0,0),(2,1,1,1,3,4,0,0,0),(3,1,1,2,1,3,0,0,0),(4,1,1,2,2,4,0,0,0),(5,1,1,3,1,4,0,0,0),(6,1,1,3,2,3,0,0,0),(7,1,2,1,5,6,0,0,0),(8,1,2,1,7,8,0,0,0),(9,1,2,2,5,7,0,0,0),(10,1,2,2,6,8,0,0,0),(11,1,2,3,5,8,0,0,0),(12,1,2,3,6,7,0,0,0),(13,1,3,1,9,10,0,0,0),(14,1,3,1,11,12,0,0,0),(15,1,3,2,9,11,0,0,0),(16,1,3,2,10,12,0,0,0),(17,1,3,3,9,12,0,0,0),(18,1,3,3,10,11,0,0,0),(19,1,4,1,13,14,0,0,0),(20,1,4,1,15,16,0,0,0),(21,1,4,2,13,15,0,0,0),(22,1,4,2,14,16,0,0,0),(23,1,4,3,13,16,0,0,0),(24,1,4,3,14,15,0,0,0);
-
-/*Data for the table `competition_season` */
-
-insert  into `competition_season`(`cs_comp`,`cs_season`) values (1,1),(2,1),(3,1),(4,1),(5,1);
-
-/*Data for the table `competition_table` */
-
-insert  into `competition_table`(`ctb_id`,`ctb_competition`,`ctb_season`,`ctb_team`,`ctb_played`,`ctb_won`,`ctb_drawn`,`ctb_lost`,`ctb_points`,`ctb_score_for`,`ctb_score_against`,`ctb_score_diff`) values (1,1,1,1,0,0,0,0,0,0,0,0),(2,1,1,2,0,0,0,0,0,0,0,0),(3,1,1,3,0,0,0,0,0,0,0,0),(4,1,1,4,0,0,0,0,0,0,0,0),(5,2,1,5,0,0,0,0,0,0,0,0),(6,2,1,6,0,0,0,0,0,0,0,0),(7,2,1,7,0,0,0,0,0,0,0,0),(8,2,1,8,0,0,0,0,0,0,0,0),(9,3,1,9,0,0,0,0,0,0,0,0),(10,3,1,10,0,0,0,0,0,0,0,0),(11,3,1,11,0,0,0,0,0,0,0,0),(12,3,1,12,0,0,0,0,0,0,0,0),(13,4,1,13,0,0,0,0,0,0,0,0),(14,4,1,14,0,0,0,0,0,0,0,0),(15,4,1,15,0,0,0,0,0,0,0,0),(16,4,1,16,0,0,0,0,0,0,0,0);
 
 /*Data for the table `competition_team` */
 
-insert  into `competition_team`(`ctm_id`,`ctm_comp`,`ctm_team`) values (1,1,1),(2,1,2),(3,1,3),(4,1,4),(5,2,5),(6,2,6),(7,2,7),(8,2,8),(9,3,9),(10,3,10),(11,3,11),(12,3,12),(13,4,13),(14,4,14),(15,4,15),(16,4,16),(17,5,1),(18,5,2),(19,5,3),(20,5,4),(21,5,5),(22,5,6),(23,5,7),(24,5,8),(25,5,9),(26,5,10),(27,5,11),(28,5,12),(29,5,13),(30,5,14),(31,5,15),(32,5,16);
+insert  into `competition_team`(`ctm_id`,`ctm_season_instance`,`ctm_competition`,`ctm_team`,`ctm_played`,`ctm_won`,`ctm_drawn`,`ctm_lost`,`ctm_points`,`ctm_score_for`,`ctm_score_against`,`ctm_score_diff`,`ctm_current_pos`,`ctm_previous_pos`,`ctm_promoted`,`ctm_relegated`,`ctm_winners`,`ctm_runners_up`,`ctm_knocked_out_round`) values (1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(2,1,1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(3,1,1,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(4,1,1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(5,1,1,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(6,1,2,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(7,1,2,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(8,1,2,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(9,1,2,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(10,1,3,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(11,1,3,11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(12,1,3,12,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(13,1,3,13,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(14,1,4,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(15,1,4,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(16,1,4,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(17,1,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(18,1,4,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(19,1,4,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(20,1,4,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(21,1,4,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(22,1,4,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(23,1,4,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(24,1,4,11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(25,1,4,12,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL),(26,1,4,13,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL);
 
-/*Data for the table `season` */
+/*Data for the table `competition_type` */
 
-insert  into `season`(`seas_id`,`seas_owner`,`seas_name`) values (1,1,'March 2014');
+insert  into `competition_type`(`comp_type_id`,`comp_name`,`comp_rank`,`comp_owner`,`comp_format`,`comp_total_places`,`comp_promo_places`,`comp_releg_places`,`comp_promo_into`,`comp_releg_into`,`comp_points_win`,`comp_points_draw`,`comp_points_lose`,`comp_rounds`,`comp_rules`) values (1,'SSDM Premier',1,1,'LEAGUE',5,0,1,NULL,2,3,1,0,5,'5 Min/Half, Game speed Normal, Results after 90 mins'),(2,'SSDM Championship',2,1,'LEAGUE',4,1,1,1,3,3,1,0,4,'5 Min/Half, Game speed Normal, Results after 90 mins'),(3,'SSDM Conference',3,1,'LEAGUE',4,1,0,2,NULL,3,1,0,4,'5 Min/Half, Game speed Normal, Results after 90 mins'),(4,'SSDM Cup',4,1,'KO',13,0,0,NULL,NULL,0,0,0,5,'5 Min/Half, Game speed Normal, Extra time if a draw after 90 min, Penalties if a draw AET.');
+
+/*Data for the table `season_instance` */
+
+insert  into `season_instance`(`sin_id`,`sin_season_type`,`sin_began`,`sin_ended`) values (1,1,'2014-03-15 00:00:00',NULL);
+
+/*Data for the table `season_type` */
+
+insert  into `season_type`(`seas_type_id`,`seas_owner`,`seas_name`,`seas_current_id`) values (1,1,'SSDM Development',2);
+
+/*Data for the table `season_type_competition` */
+
+insert  into `season_type_competition`(`stc_id`,`stc_season_type`,`stc_competition`) values (1,1,1),(2,1,2),(3,1,3),(4,1,4);
 
 /*Data for the table `team` */
 
-insert  into `team`(`team_id`,`team_owner`,`team_name`,`team_created`,`team_updated`) values (1,1,'Prem Team 1','2014-07-03 18:30:00','2014-07-03 18:30:00'),(2,1,'Prem Team 2','2014-07-03 18:30:00','2014-07-03 18:30:00'),(3,1,'Prem Team 3','2014-07-03 18:30:00','2014-07-03 18:30:00'),(4,1,'Prem Team 4','2014-07-03 18:30:00','2014-07-03 18:30:00'),(5,1,'Chmp Team 1','2014-07-03 18:30:00','2014-07-03 18:30:00'),(6,1,'Chmp Team 2','2014-07-03 18:30:00','2014-07-03 18:30:00'),(7,1,'Chmp Team 3','2014-07-03 18:30:00','2014-07-03 18:30:00'),(8,1,'Chmp Team 4','2014-07-03 18:30:00','2014-07-03 18:30:00'),(9,1,'L1 Team 1','2014-07-03 18:30:00','2014-07-03 18:30:00'),(10,1,'L1 Team 2','2014-07-03 18:30:00','2014-07-03 18:30:00'),(11,1,'L1 Team 3','2014-07-03 18:30:00','2014-07-03 18:30:00'),(12,1,'L1 Team 4','2014-07-03 18:30:00','2014-07-03 18:30:00'),(13,1,'L2 Team 1','2014-07-03 18:30:00','2014-07-03 18:30:00'),(14,1,'L2 Team 2','2014-07-03 18:30:00','2014-07-03 18:30:00'),(15,1,'L2 Team 3','2014-07-03 18:30:00','2014-07-03 18:30:00'),(16,1,'L2 Team 4','2014-07-03 18:30:00','2014-07-03 18:30:00');
+insert  into `team`(`team_id`,`team_owner`,`team_name`,`team_created`,`team_updated`) values (1,1,'Dave Keywood','2014-07-03 18:30:00','2014-07-03 18:30:00'),(2,1,'Mark Kitching','2014-07-03 18:30:00','2014-07-03 18:30:00'),(3,1,'Paul Grave','2014-07-03 18:30:00','2014-07-03 18:30:00'),(4,1,'Dave Gordon','2014-07-03 18:30:00','2014-07-03 18:30:00'),(5,1,'Nick Philpott','2014-07-03 18:30:00','2014-07-03 18:30:00'),(6,1,'Gary Ward','2014-07-03 18:30:00','2014-07-03 18:30:00'),(7,1,'Michael Cooper','2014-07-03 18:30:00','2014-07-03 18:30:00'),(8,1,'Chris Booker','2014-07-03 18:30:00','2014-07-03 18:30:00'),(9,1,'Chris Bell','2014-07-03 18:30:00','2014-07-03 18:30:00'),(10,1,'Amy Bamber','2014-07-03 18:30:00','2014-07-03 18:30:00'),(11,1,'Guy Taylor','2014-07-03 18:30:00','2014-07-03 18:30:00'),(12,1,'Ryan Winfield','2014-07-03 18:30:00','2014-07-03 18:30:00'),(13,1,'Jamie Hobbs','2014-07-03 18:30:00','2014-07-03 18:30:00');
+
+/*Data for the table `user_account` */
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
--- schema/structure-only.sql
+++ schema/structure-only.sql
@@ -12,17 +12,6 @@ MySQL - 5.0.67-community-nt : Database - office_league
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*Table structure for table `competition` */
-
-DROP TABLE IF EXISTS `competition`;
-
-CREATE TABLE `competition` (
-  `comp_id` int(6) NOT NULL auto_increment,
-  `comp_name` char(64) NOT NULL,
-  `comp_owner` int(6) NOT NULL,
-  PRIMARY KEY  (`comp_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-
 /*Table structure for table `competition_fixture` */
 
 DROP TABLE IF EXISTS `competition_fixture`;
@@ -38,36 +27,7 @@ CREATE TABLE `competition_fixture` (
   `fix_home_score` int(2) NOT NULL default '0',
   `fix_away_score` int(2) NOT NULL default '0',
   PRIMARY KEY  (`fix_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
-
-/*Table structure for table `competition_season` */
-
-DROP TABLE IF EXISTS `competition_season`;
-
-CREATE TABLE `competition_season` (
-  `cs_comp` int(6) NOT NULL,
-  `cs_season` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Table structure for table `competition_table` */
-
-DROP TABLE IF EXISTS `competition_table`;
-
-CREATE TABLE `competition_table` (
-  `ctb_id` int(6) NOT NULL auto_increment,
-  `ctb_competition` int(6) NOT NULL,
-  `ctb_season` int(6) NOT NULL,
-  `ctb_team` int(6) NOT NULL,
-  `ctb_played` int(6) NOT NULL default '0',
-  `ctb_won` int(2) NOT NULL default '0',
-  `ctb_drawn` int(2) NOT NULL default '0',
-  `ctb_lost` int(2) NOT NULL default '0',
-  `ctb_points` int(3) NOT NULL default '0',
-  `ctb_score_for` int(3) NOT NULL default '0',
-  `ctb_score_against` int(3) NOT NULL default '0',
-  `ctb_score_diff` int(3) NOT NULL default '0',
-  PRIMARY KEY  (`ctb_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `competition_team` */
 
@@ -75,21 +35,84 @@ DROP TABLE IF EXISTS `competition_team`;
 
 CREATE TABLE `competition_team` (
   `ctm_id` int(6) NOT NULL auto_increment,
-  `ctm_comp` int(6) NOT NULL,
+  `ctm_season_instance` int(6) NOT NULL,
+  `ctm_competition` int(6) NOT NULL,
   `ctm_team` int(6) NOT NULL,
+  `ctm_played` int(2) NOT NULL default '0',
+  `ctm_won` int(2) NOT NULL default '0',
+  `ctm_drawn` int(2) NOT NULL default '0',
+  `ctm_lost` int(2) NOT NULL default '0',
+  `ctm_points` int(3) NOT NULL default '0',
+  `ctm_score_for` int(3) NOT NULL default '0',
+  `ctm_score_against` int(3) NOT NULL default '0',
+  `ctm_score_diff` int(3) NOT NULL default '0',
+  `ctm_current_pos` int(2) NOT NULL default '0',
+  `ctm_previous_pos` int(2) NOT NULL default '0',
+  `ctm_promoted` tinyint(1) NOT NULL default '0',
+  `ctm_relegated` tinyint(1) NOT NULL default '0',
+  `ctm_winners` tinyint(1) NOT NULL default '0',
+  `ctm_runners_up` tinyint(1) NOT NULL default '0',
+  `ctm_knocked_out_round` int(2) default NULL,
   PRIMARY KEY  (`ctm_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
 
-/*Table structure for table `season` */
+/*Table structure for table `competition_type` */
 
-DROP TABLE IF EXISTS `season`;
+DROP TABLE IF EXISTS `competition_type`;
 
-CREATE TABLE `season` (
-  `seas_id` int(6) NOT NULL auto_increment,
+CREATE TABLE `competition_type` (
+  `comp_type_id` int(6) NOT NULL auto_increment,
+  `comp_name` char(64) NOT NULL,
+  `comp_rank` int(3) NOT NULL default '1',
+  `comp_owner` int(6) NOT NULL default '1',
+  `comp_format` char(6) NOT NULL default 'LEAGUE',
+  `comp_total_places` int(3) NOT NULL default '0',
+  `comp_promo_places` int(2) NOT NULL default '0',
+  `comp_releg_places` int(2) NOT NULL default '0',
+  `comp_promo_into` int(6) default NULL,
+  `comp_releg_into` int(6) default NULL,
+  `comp_points_win` int(1) NOT NULL default '3',
+  `comp_points_draw` int(1) NOT NULL default '1',
+  `comp_points_lose` int(1) NOT NULL default '0',
+  `comp_rounds` int(2) default '0',
+  `comp_rules` char(255) default NULL,
+  PRIMARY KEY  (`comp_type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `season_instance` */
+
+DROP TABLE IF EXISTS `season_instance`;
+
+CREATE TABLE `season_instance` (
+  `sin_id` int(6) NOT NULL auto_increment,
+  `sin_season_type` int(5) NOT NULL,
+  `sin_began` datetime default NULL,
+  `sin_ended` datetime default NULL,
+  PRIMARY KEY  (`sin_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `season_type` */
+
+DROP TABLE IF EXISTS `season_type`;
+
+CREATE TABLE `season_type` (
+  `seas_type_id` int(6) NOT NULL auto_increment,
   `seas_owner` int(6) NOT NULL,
   `seas_name` char(64) NOT NULL,
-  PRIMARY KEY  (`seas_id`)
+  `seas_current_id` int(6) NOT NULL default '0',
+  PRIMARY KEY  (`seas_type_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `season_type_competition` */
+
+DROP TABLE IF EXISTS `season_type_competition`;
+
+CREATE TABLE `season_type_competition` (
+  `stc_id` int(6) NOT NULL auto_increment,
+  `stc_season_type` int(6) NOT NULL,
+  `stc_competition` int(6) NOT NULL,
+  PRIMARY KEY  (`stc_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `team` */
 
@@ -102,7 +125,7 @@ CREATE TABLE `team` (
   `team_created` datetime NOT NULL,
   `team_updated` datetime default NULL,
   PRIMARY KEY  (`team_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `user_account` */
 
@@ -117,7 +140,7 @@ CREATE TABLE `user_account` (
   `uac_lastvisit_date` datetime NOT NULL,
   `uac_lastvisit_ip` char(32) NOT NULL,
   PRIMARY KEY  (`uac_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
